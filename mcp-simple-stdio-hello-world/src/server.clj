aad669a20c24a697cb4a9cca52d68f3153df5990
Dan <dan@eighttrigrams.net>
2025-07-27 19:33:59 +0100

    Handle notifications/initialized

--- mcp-simple-stdio-hello-world/src/server.clj
+++ mcp-simple-stdio-hello-world/src/server.clj
@@ -39,44 +39,47 @@
 (defn handle-request [request]
   (let [method (:method request)
         params (:params request)]
-    (merge 
-     {:jsonrpc "2.0"}
-     (cond
-       (= method "initialize")
-       {:result {:protocolVersion "2024-11-05"
-                 :capabilities    {:tools {}}  ;; Just declare "we have tools"
-                 :serverInfo      {:name    "weather-mcp"
-                                   :version "1.0.0"}}}
-       (= method "tools/list")
-       {:result  {:tools (mapv second tools-list)}}
-       (= method "tools/call")
-       (let [tool-name (get-in params [:name])
-             arguments (get-in params [:arguments])
-             f (map-tool tool-name)]
-         (if f 
-           (let [result (try (f arguments)
-                             (catch IllegalArgumentException e
-                               {:error {:code    -32602
-                                        :message (.getMessage e)}})
-                             (catch UnsupportedOperationException e
-                               {:error {:code    -32801
-                                        :message (.getMessage e)}}))]
-             {:result  {:content [{:type "text"
-                                   :text (json/generate-string result)}]}}) 
-           {:error   {:code    -32601
-                      :message "Unknown tool"}}))
-       :else
-       {:error   {:code    -32601
-                  :message "Unknown method"}}))))
+    (if (= method "notifications/initialized")
+      nil
+      (merge 
+       {:jsonrpc "2.0"}
+       (cond
+         (= method "initialize")
+         {:result {:protocolVersion "2024-11-05"
+                   :capabilities    {:tools {}}  ;; Just declare "we have tools"
+                   :serverInfo      {:name    "weather-mcp"
+                                     :version "1.0.0"}}}
+         (= method "tools/list")
+         {:result  {:tools (mapv second tools-list)}}
+         (= method "tools/call")
+         (let [tool-name (get-in params [:name])
+               arguments (get-in params [:arguments])
+               f (map-tool tool-name)]
+           (if f 
+             (let [result (try (f arguments)
+                               (catch IllegalArgumentException e
+                                 {:error {:code    -32602
+                                          :message (.getMessage e)}})
+                               (catch UnsupportedOperationException e
+                                 {:error {:code    -32801
+                                          :message (.getMessage e)}}))]
+               {:result  {:content [{:type "text"
+                                     :text (json/generate-string result)}]}}) 
+             {:error   {:code    -32601
+                        :message "Unknown tool"}}))
+         :else
+         {:error   {:code    -32601
+                    :message "Unknown method"}})))))
 
 (defn process-line [line]
   (when-not (empty? line)
     (try
       (let [request (json/parse-string line true)
-            response (handle-request request)
-            response-with-id (assoc response :id (:id request))]
-        (println (json/generate-string response-with-id))
-        (flush))
+            response (handle-request request)]
+        (when response  ; Only respond if there's a response (not for notifications)
+          (let [response-with-id (assoc response :id (:id request))]
+            (println (json/generate-string response-with-id))
+            (flush))))
       (catch Exception _e
         (println (json/generate-string {:jsonrpc "2.0"
                                         :error {:code -32700
